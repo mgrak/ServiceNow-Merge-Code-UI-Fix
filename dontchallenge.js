662694785242932f38fac41ec0a6dbc25ff18b80
blindinglightvr <37604982+blindinglightvr@users.noreply.github.com>
2022-03-31 14:14:46 -0700

    Got checkbox working without reload

--- dontchallenge.js
+++ dontchallenge.js
@@ -1,7 +1,9 @@
+console.clear();
+console.log('dontchallenge start');
 var id = setInterval(function(){
 if (document.querySelectorAll("#remember_device_div > div > label").length > 0){
 	console.log('check');
 	document.querySelectorAll("#remember_device_div > div > label")[0].click();
 	clearInterval(id);
 }
-}, 100); 
+}, 500); 
